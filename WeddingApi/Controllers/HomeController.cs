49c7d0e4098870631d8a24030fede8d125ab3aa2
Mousa Azimi <mousa.azimi77@gmail.com>
2021-05-22 01:54:32 +0200

    register/login(wd-guest)  form page

--- WeddingApi/Controllers/HomeController.cs
+++ WeddingApi/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace WeddingApi.Controllers
             return View();
         }
 
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
--- WeddingApi/wwwroot/js/site.js
+++ WeddingApi/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+jQuery(document).ready(function ($) {
+	tab = $('.tabs h5 a');
+
+	tab.on('click', function (event) {
+		event.preventDefault();
+		tab.removeClass('active');
+		$(this).addClass('active');
+
+		tab_content = $(this).attr('href');
+		$('div[id$="tab-content"]').removeClass('active');
+		$(tab_content).addClass('active');
+	});
+});
