659816db42d6469b8741127c479cc21229379796
Robert Sörensen <robert.sorensen44@gmail.com>
2021-05-21 11:47:17 +0200

    first push. db couples

---
+++ WeddingApi/Data/WeddingDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeddingApi.Model.Couple;
+
+namespace WeddingApi.Data
+{
+    public class WeddingDbContext : IdentityDbContext
+    {
+        public DbSet<WeddingCouple> Couple { get; set; }
+    }
+}
---
+++ WeddingApi/Model/Couple/Person.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeddingApi.Model.Couple
+{
+    public class Person : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string SocialSecurityNumber { get; set; }
+    }
+}
