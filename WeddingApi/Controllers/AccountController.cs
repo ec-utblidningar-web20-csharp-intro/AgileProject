75fe6acd46ddf0215d1984971ca658f373047df9
Palmeuh <fredrik.palme@utb.ecutbildning.se>
2021-05-27 16:11:33 +0200

    Implementerat enkel registrering samt att man loggas in om det lyckas. Ett antal funderingar. 1. Behöver vi all den datan i en <Person>? Enligt mig kan den bantas rätt kraftigt och ha kvar namn, email, gender och lösenord. 2. I dagsläget ingen validering på uppgifterna som skrivs in samt några felmeddelande/krav vid ifyllnad vid registrering, t.ex. "password not long enough" m.m.. Sammanfattning att göra i detta issue: 1. Inloggning med autentisering. 2. Validering av alla uppgifter som fylls i på inlogg/registrering. Vid lyckad registrering sker inlogg och länkning till dashboard, för nävarande hamnar man åter på registreringssidan.

--- WeddingApi/Controllers/AccountController.cs
+++ WeddingApi/Controllers/AccountController.cs
@@ -3,19 +3,53 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using WeddingApi.Data;
 using WeddingApi.Models.Couple;
+using WeddingApi.ViewModels;
 
 namespace WeddingApi.Controllers
 {
     public class AccountController : Controller
     {
-     
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public class RegisterInput
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        
+
+
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([Bind("FirstName, LastName," +
+            "Email, Password, PhoneNumber")] RegisterPersonViewModel input)
+        {
+            var person = new Person { UserName = input.Email, Email = input.Email };
+            var tryCreateNewPerson = await _userManager.CreateAsync(person, input.Password);
+            if (tryCreateNewPerson.Succeeded)
+            {
+                await _signInManager.SignInAsync(person, false);
+                //någon annan view här. 
+                return View("Register");
+            }
+            return View("Register");
         }
 
         public IActionResult Login()
@@ -23,5 +57,7 @@ namespace WeddingApi.Controllers
             return View();
         }
 
+        
     }
+
 }
---
+++ WeddingApi/ViewModels/RegisterPersonViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeddingApi.ViewModels
+{
+    public class RegisterPersonViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string PersonNumber { get; set; }
+        public string Gender { get; set; }
+        public string SocialSecurityNumber { get; set; }
+        public string Address { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
