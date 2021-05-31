7044d3a6562ab0e123a6c46c4b7aa971416e6948
Robert Sörensen <robert.sorensen44@gmail.com>
2021-05-31 16:47:14 +0200

    strul me guest. me join wedding

--- WeddingApi/Data/TestSeed.cs
+++ WeddingApi/Data/TestSeed.cs
@@ -18,7 +18,7 @@ namespace WeddingApi.Data
 
         public static Random jadu = new Random();
 
-        
+
         public async static Task Seeder(IServiceProvider services)
         {
             _context = services.GetRequiredService<WeddingDbContext>();
@@ -29,15 +29,12 @@ namespace WeddingApi.Data
 
             await TestSeedWedding();
         }
+
         public List<WeddingCouple> couples { get; set; }
         static async Task TestSeedWedding()
         {
-
-
-            
             var FilePathPerson = @".\MOCK_DATA_2.json";
             var Personas = JsonConvert.DeserializeObject<List<Person>>(File.ReadAllText(FilePathPerson));
-
             foreach (var person in Personas)
             {
                 await _userManager.CreateAsync(person, "test123!");
@@ -45,9 +42,6 @@ namespace WeddingApi.Data
 
             var FilePathWedding = @".\MOCK_DATA.json";
             var Weddings = JsonConvert.DeserializeObject<List<Wedding>>(File.ReadAllText(FilePathWedding));
-            
-           
-
             for (int i = 0; i < 500; i++)
             {
                 WeddingCouple weddingCouple = new WeddingCouple()
@@ -56,9 +50,9 @@ namespace WeddingApi.Data
                     {
                         Personas[i],
                         Personas[i + 500]
-                    }           
+                    }
                 };
-              
+
                 await _context.AddAsync(weddingCouple);
                 await _context.SaveChangesAsync();
             };
@@ -67,46 +61,44 @@ namespace WeddingApi.Data
             for (int i = 0; i < Weddings.Count / 2; i++)
             {
                 var test = _context.Couple.Find(i + 1);
-                Weddings[i].CoupleId = test.Id;
                 Weddings[i].Couple = test;
-
-
-
                 await _context.Wedding.AddAsync(Weddings[i]);
             }
 
-
-
-
             await _context.SaveChangesAsync();
-
-
 
             var FilePathMockNames = @".\MOCK_DATA_14.json";
-             var MockNames = JsonConvert.DeserializeObject<List<Guests>>(File.ReadAllText(FilePathMockNames));
+            var guests = JsonConvert.DeserializeObject<List<Guests>>(File.ReadAllText(FilePathMockNames));
             var weddings = _context.Wedding.AsNoTracking().ToList();
+            var guestIterator = 0;
             for (int c = 0; c < weddings.Count; c++)
             {
-                var bröllop = weddings[c];
-            for (int i = 0; i < MockNames.Count; i++)
-            {
-                MockNames[i].FriendsOrFamily = (FriendsOrFamily)jadu.Next(0, 2);
-                MockNames[i].Answer = (Status)jadu.Next(0, 4);
-                MockNames[i].Side = (MarrierSide)jadu.Next(0, 3);
-                var folktillbröllop = jadu.Next(50, 100);
-               
-                for (int j = 0; j < folktillbröllop; j++)
-                {
-                    MockNames[j].JoinedWedding = bröllop;
-                }
-                
-                await _context.AddAsync(MockNames[i]);
-            }
+                var invitees = jadu.Next(50, 100);
 
+                for (int j = 0; j < invitees; j++)
+                {
+                    var guestEntity = guests.Skip(guestIterator).First();
+                    guestEntity.JoinedWedding = weddings[c];
+                    guestEntity.FriendsOrFamily = (FriendsOrFamily)jadu.Next(0, 2);
+                    guestEntity.Answer = (Status)jadu.Next(0, 4);
+                    guestEntity.Side = (MarrierSide)jadu.Next(0, 3);
+
+                    await _context.AddAsync(guestEntity);
+
+                    guestIterator = guestIterator >= guests.Count ? 0 : guestIterator++;
+                }
             }
             await _context.SaveChangesAsync();
 
-            
+
+            //var folktillbröllop = jadu.Next(50, 100);
+
+            //for (int j = 0; j < folktillbröllop; j++)
+            //{
+            //    MockNames[j].JoinedWedding = bröllop;
+            //}
+
+
 
         }
     }
