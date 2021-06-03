00cd8dbde6eef68914b4fb45273de809969eaea0
Robin Grabs <70430807+3grabs7@users.noreply.github.com>
2021-06-03 16:11:11 +0200

    Revert "fixed main up to date based on last sprint (#35)"
    This reverts commit abbfe47a6836b0d9d2cadd7c7f9aecd18f3898c3.

---
+++ WeddingApi/Models/GuestTableImg/GuestTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeddingApi.Models.Couple;
+
+namespace WeddingApi.Models.GuestTableImg
+{
+    public class GuestTable
+    {
+        public int Id { get; set; }
+
+        public string TableName { get; set; }
+
+        public string  URLImage{ get; set; }
+
+    }
+}
