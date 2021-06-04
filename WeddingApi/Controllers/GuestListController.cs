a1018c938a2d91212f274b4285b4b380cf29d122
Robert Sörensen <robert.sorensen44@gmail.com>
2021-06-04 16:40:33 +0200

    Lista av guests beroende på id av wedding

---
+++ WeddingApi/Controllers/GuestListController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WeddingApi.Data;
+using WeddingApi.Models;
+
+namespace WeddingApi.Controllers
+{
+    public class GuestListController : Controller
+    {
+        private readonly WeddingDbContext _context;
+
+        public GuestListController(WeddingDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: GuestList
+        public async Task<IActionResult> Index(int? id)
+        {
+            var GetWeddingId = id;
+            var GetWedding = await _context.Weddings
+                .Where(m => m.Id == GetWeddingId)
+                .FirstOrDefaultAsync();
+
+            return View(await _context.Guests
+                .Where(m => m.JoinedWedding == GetWedding)
+                .ToListAsync());
+        }
+
+        // GET: GuestList/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var guest = await _context.Guests
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (guest == null)
+            {
+                return NotFound();
+            }
+
+            return View(guest);
+        }
+
+        // GET: GuestList/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GuestList/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Gender,Email,Country,City,Allergies,AmountKids,Side,FriendsOrFamily,Answer,HasPlusOne,NeedTransportation,NeedLodging")] Guest guest)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(guest);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(guest);
+        }
+
+        // GET: GuestList/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var guest = await _context.Guests.FindAsync(id);
+            if (guest == null)
+            {
+                return NotFound();
+            }
+            return View(guest);
+        }
+
+        // POST: GuestList/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Gender,Email,Country,City,Allergies,AmountKids,Side,FriendsOrFamily,Answer,HasPlusOne,NeedTransportation,NeedLodging")] Guest guest)
+        {
+            if (id != guest.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(guest);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GuestExists(guest.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(guest);
+        }
+
+        // GET: GuestList/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var guest = await _context.Guests
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (guest == null)
+            {
+                return NotFound();
+            }
+
+            return View(guest);
+        }
+
+        // POST: GuestList/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var guest = await _context.Guests.FindAsync(id);
+            _context.Guests.Remove(guest);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GuestExists(int id)
+        {
+            return _context.Guests.Any(e => e.Id == id);
+        }
+    }
+}
